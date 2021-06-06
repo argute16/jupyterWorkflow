2eb6c136cb78f5cfeae32b3012ff3867c1381cf5
argute16 <83342017+argute16@users.noreply.github.com>
2021-06-06 14:02:43 +0200

    Add files via upload

---
+++ jupyterworkflow/data.py
@@ -0,0 +1,28 @@
+import os
+from urllib.request import urlretrieve
+import pandas as pd
+
+Fremont_URL = 'https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD'
+
+
+def get_fremont_data(filename='Fremont.csv', url=Fremont_URL, force_download=False):
+    """Download and cache the fremont data
+    Parameters:
+    --------
+    filename: string (optional)
+        location to safe the data
+    url: string (optional)
+        weblocation of the data
+    force_download: bool (optional)
+        if True, force redownload of data
+    Returns:
+    --------
+    data: pandas.DataFrame
+        The fremont bridge data
+    """
+    
+    if force_download or not os.path.exists(filename):
+        urlretrieve(url, filename)
+    data = pd.read_csv(filename, index_col='Date', parse_dates=True)
+    data.columns = ['Total', 'West', 'East']
+    return data
